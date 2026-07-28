832b8710ae34b33c1239d691ca0472b3c1dd61cd
Hugo Lomba <98366477+hugolomba@users.noreply.github.com>
2026-07-29 00:09:30 +0100

    wire layout into routes and add interviews/cvs routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
 import ProtectedRoute from './routes/ProtectedRoute'
+import Layout from './components/Layout'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Dashboard from './pages/Dashboard'
@@ -8,7 +9,16 @@ import Dashboard from './pages/Dashboard'
 import Applications from './pages/Applications'
 import ApplicationForm from './pages/ApplicationForm'
 import ApplicationDetail from './pages/ApplicationDetail'
+import Interviews from './pages/Interviews'
+import CvManager from './pages/CvManager'
 
+function Protected({ children }) {
+  return (
+    <ProtectedRoute>
+      <Layout>{children}</Layout>
+    </ProtectedRoute>
+  )
+}
 
 export default function App() {
   return (
@@ -18,18 +28,13 @@ export default function App() {
           <Route path="/" element={<Navigate to="/dashboard" replace />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route
-            path="/dashboard"
-            element={
-              <ProtectedRoute>
-                <Dashboard />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="/applications" element={<ProtectedRoute><Applications /></ProtectedRoute>} />
-          <Route path="/applications/new" element={<ProtectedRoute><ApplicationForm /></ProtectedRoute>} />
-          <Route path="/applications/:id/edit" element={<ProtectedRoute><ApplicationForm /></ProtectedRoute>} />
-          <Route path="/applications/:id" element={<ProtectedRoute><ApplicationDetail /></ProtectedRoute>} />
+          <Route path="/dashboard" element={<Protected><Dashboard /></Protected>} />
+          <Route path="/applications" element={<Protected><Applications /></Protected>} />
+          <Route path="/applications/new" element={<Protected><ApplicationForm /></Protected>} />
+          <Route path="/applications/:id/edit" element={<Protected><ApplicationForm /></Protected>} />
+          <Route path="/applications/:id" element={<Protected><ApplicationDetail /></Protected>} />
+          <Route path="/interviews" element={<Protected><Interviews /></Protected>} />
+          <Route path="/cvs" element={<Protected><CvManager /></Protected>} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
